b5a3f9ef26d7669f0afd289485771c1830aaa3b7
samuele.mattiuzzo <samumatt@gmail.com>
2012-10-21 22:20:43 +0200

    Problem 5

---
+++ 005.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os, sys
+
+def solve():
+        print '''
+                Problem 4:
+		2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+		What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+	'''
+
+	RES = 2520
+	
+	while True:
+		test = True
+		for i in range(1,21):
+			if RES%i!=0:
+				test = False
+				break
+		if test == True:
+			break
+		else:
+			test = True
+			RES += 1
+	return RES
+
+def main():
+        res = solve()
+        print "Solution: %s " % (str(res))
+
+if __name__=="__main__":
+        main()
+
