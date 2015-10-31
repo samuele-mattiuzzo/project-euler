5106ede64201acc9ec6b40fd03681172f8965523
Samuele Mattiuzzo <samumatt@gmail.com>
2015-10-31 04:00:13 +0000

    Preparing the files for the next 4 problems I'll tackle

---
+++ 031.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os, sys
+
+def solve():
+	print '''
+		[ copy here the problem's text ]
+	'''
+
+	# main body
+	return 0
+
+
+def main():
+	res = solve()
+	print "Solution: %s " % (str(res))
+
+if __name__=="__main__":
+	main()
