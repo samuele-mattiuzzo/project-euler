d9d45884f0e512bb83c0c29b4a5106ac1b18e1e4
Samuele Mattiuzzo <smattiuzzo@google.com>
2015-10-31 02:57:56 +0000

    Polishing up old files

--- 001.py
+++ 001.py
@@ -4,12 +4,13 @@ import os, sys
 
 def solve():
 	print '''
-		Problem 1:
-		If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+		Problem 1
+
+		If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 		The sum of these multiples is 23.
 		Find the sum of all the multiples of 3 or 5 below 1000.
 	'''
-	
+
 	MAX = 1000
 	SUM = 0
 
--- 005.py
+++ 005.py
@@ -3,15 +3,16 @@
 import os, sys
 
 def solve():
-        print '''
-                Problem 4:
+    print '''
+        Problem 5
+
 		2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 		What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 	'''
 
 	RES = 2520
-	
+
 	while True:
 		test = True
 		for i in range(1,21):
@@ -26,9 +27,8 @@ def solve():
 	return RES
 
 def main():
-        res = solve()
-        print "Solution: %s " % (str(res))
+    res = solve()
+    print "Solution: %s " % (str(res))
 
 if __name__=="__main__":
-        main()
-
+    main()
